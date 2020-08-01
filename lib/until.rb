746bb0a3bea943ea3a73d2b4201d6b2d8416cf8f
Bria White <bria.awhite@gmail.com>
2020-08-01 01:13:37 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,11 +1,9 @@
 def using_until
   levitation_force = 6
   
-  while levitation_force = levitation + 1 >= 10
-  
-  if levitation_force >= 10
-
-  end 
+  while levitation_force >= 10
+    puts levitation_force
+    levitation_force += 10
 end
 
 puts "Wingardium Leviosa"
